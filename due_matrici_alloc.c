97d2fa3af76ae9279b008e4dcee34a45d2fe1c82
Poliseno Riccardo <polisenoriccardo6@gmail.com>
2026-01-21 12:53:09 +0100

    esercizio moltiplicazione matrici finito

--- due_matrici_alloc.c
+++ due_matrici_alloc.c
@@ -29,8 +29,20 @@ void freeMatrix(int **mat, int rows) {
 void fillMatrix(int** mat, int rows, int cols) {
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
-      printf("Inserisci il valore per la cella %d-%d", rows, cols);
-      scanf("%d", mat[i][j]);
+      printf("Inserisci il valore per la cella %d-%d",i, j);
+      scanf("%d",&mat[i][j]);
+    }
+  }
+}
+
+
+void moltMatrix(int** matriceA, int** matriceB,int** matriceC, int r1, int c1, int c2) {
+  for (int i = 0; i < r1; i++) {
+    for (int j = 0; j < c2; j++) {
+      matriceC[i][j] = 0;
+      for (int k = 0; k < c1; k++) {
+         matriceC[i][j] += matriceA[i][k] * matriceB[k][j];
+      }
     }
   }
 }
@@ -59,8 +71,22 @@ int main() {
   int** matriceB = allocMatrix(r2, c2);
   int** matriceC = allocMatrix(r1, c2);
 
-  fillMatrice(matriceA, r1, c1);
-  fillMatrice(matriceB, r2, c2);
+  fillMatrix(matriceA, r1, c1);
+  fillMatrix(matriceB, r2, c2);
+
+  moltMatrix(matriceA, matriceB, matriceC, r1, c1, c2);
+
+
+  printf("Matrice A: \n");
+  printMatrix(matriceA, r1, c1);
+  printf("Matrice B: \n");
+  printMatrix(matriceB, r2, c2);
+  printf("Matrice C: \n");
+  printMatrix(matriceC, r1, c2);
   
+  freeMatrix(matriceA, r1);
+  freeMatrix(matriceB, r2);
+  freeMatrix(matriceC, r1);
+
   return 0;
 }
